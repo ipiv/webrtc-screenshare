1cc5f25a6e25fc9f3ec68f261189d630db5c1936
ipiv <hello.ipiv@gmail.com>
2019-12-13 19:26:55 +0100

    very rough wip

---
+++ src/main.js
@@ -0,0 +1,185 @@
+const swarm = require('webrtc-swarm')
+const signalhub = require('signalhub')
+const Peer = require('simple-peer')
+
+const hub = signalhub('sharescreen', ['https://signalhubb.herokuapp.com/'])
+
+const createRoom = async () => {
+  console.log('Creating new channel...')
+  const roomId = randomId()
+  const identifier = randomId()
+  await joinHub(roomId, identifier, true)
+  createRoomInfoEl(roomId)
+  // const peer = await initPeer(roomId, true)
+}
+
+const createRoomInfoEl = (roomId) => {
+  const roomInfoEl = document.createElement('div')
+  const infoH2 = document.createElement('h2')
+  const infoP = document.createElement('h2')
+  infoH2.textContent = 'Your Room ID is:'
+  infoP.textContent = roomId
+  roomInfoEl.append(infoH2)
+  roomInfoEl.append(infoP)
+  document.getElementById('connect-wrapper').prepend(roomInfoEl)
+  document.getElementById('join-room-container').hidden = true
+  document.getElementById('start-buttons').hidden = true
+}
+
+const joinHub = async (roomId, id, initiator = false) => {
+  hub.subscribe(roomId)
+    .on('data', (data) => {
+      if (data.action == 'joined') {
+        const messageEl = document.createElement('li')
+        if (data.from == id) {
+          data.from += ' (You)'
+        }
+        messageEl.textContent = data.from+' - Just joined'
+        document.getElementById('messages').appendChild(messageEl)
+        
+      } else if (data.action == 'getConnected') {
+        hub.broadcast(roomId, {from: id, action:'connected'})
+
+      } else if (data.action == 'connected') {
+        const chattersEl = document.getElementById('chatters')
+        const hasChatter = chattersEl.querySelector('#chatter-'+data.from) != null;
+        if(!hasChatter) {
+          const newChatter = document.createElement('li')
+          newChatter.setAttribute('id', 'chatter-'+data.from)
+          newChatter.textContent = data.from == id ? data.from+'(You)' : data.from
+          newChatter.addEventListener('click', startScreenshare)
+          chattersEl.append(newChatter)
+        }
+      } else if (data.action == 'startScreenShare') {
+        if (data.from !== id) {
+          initializeScreenShare(true).then(peer =>{
+            hub.peer = peer
+          })
+        }
+      } else if (data.action == 'signal') {
+        if (data.from !== id) {
+          console.log('Got signalling data, sending to peer')
+          hub.peer.signal(data.signalData)
+        }
+      }
+      // peer.signal(data)
+    })
+  hub.broadcast(roomId, {from: id, action:'joined'}, () => getConnected(roomId, id))
+  hub.roomId = roomId
+  hub.identifier = id
+  return
+}
+const getConnected = (roomId, id) => {
+  hub.broadcast(roomId, {from: id, action:'getConnected'}, (data) => {
+    console.log(data)
+  })
+  // console.log(hub)
+}
+const joinRoomById = async () => {
+  const roomId = document.getElementById('room-id-input').value
+  const identifier = randomId()
+  await joinHub(roomId, identifier)
+  createRoomInfoEl(roomId)
+  getConnected(roomId, identifier)
+  // const peer = await initPeer(roomId)
+}
+
+const showJoinContainer = () => {
+  document.getElementById('join-room-container').hidden = false
+}
+
+const startScreenshare = async() => {
+  const peer = await initializeScreenShare()
+  hub.peer = peer
+  hub.broadcast(hub.roomId, {from: hub.identifier, action:'startScreenShare'})
+}
+
+const initializeScreenShare = async (initiator = false) => {
+  const stream = await navigator.mediaDevices.getDisplayMedia({ video: true, audio: true })
+  if (!stream) return new Error('Failed to get stream')
+  const peer = new Peer({ initiator, stream })
+  peer.on('signal', data => {
+    hub.broadcast(hub.roomId, {from: hub.identifier, action:'signal', signalData: data})
+  })
+  peer.on('connect', () => {
+    console.log('Peer is connected')
+  })
+  peer.on('stream', stream => {
+    const videoWrapper = document.createElement('div')
+    videoWrapper.setAttribute('id', 'video-wrapper')
+    const videoEl = document.createElement('video')
+    if ('srcObject' in videoEl) {
+      videoEl.srcObject = stream
+    } else {
+      videoEl.src = window.URL.createObjectURL(stream) // for older browsers
+    }
+    videoWrapper.append(videoEl)
+    const connectionWrapper = document.getElementById('connect-wrapper').parentNode
+    connectionWrapper.hidden = true
+    connectionWrapper.parentNode.append(videoWrapper)
+    videoEl.play()
+  })
+  // peer.identifier = randomId()
+  return peer
+}
+
+const randomId = (length = 6) => {
+  return Math.round((Math.random() * 36 ** length)).toString(36)
+}
+
+const main = () => {
+  const joinButton = document.getElementById('show-join-room')
+  joinButton.addEventListener('click', showJoinContainer)
+
+  const submitRoomButton = document.getElementById('submit-room-id')
+  submitRoomButton.addEventListener('click', joinRoomById)
+
+  const createButton = document.getElementById('create-room')
+  createButton.addEventListener('click', createRoom)
+}
+main()
+
+
+
+// const initPeer = async (roomId, initiator = false) => {
+//   const peer = await initializeScreenShare(initiator)
+
+//   hub.subscribe(roomId)
+//     .on('data', (data) => {
+//       console.log('hub | ondata',data)
+//       if(data.from !== peer.identifier) {
+//         console.log('hub | ondata - not from me')
+//       }
+//       // peer.signal(data)
+//     })
+  
+//   peer.on('signal', (data) => {
+//     const id = peer.identifier
+//     console.log('peer | onsignal',data)
+//     hub.broadcast(roomId, {from:peer.identifier, signal:data})
+//   })
+
+//   return peer
+// }
+
+
+
+// hub.subscribe('my-channel')
+//   .on('data', function (message) {
+//     console.log('new message received', message)
+//   })
+
+// hub.broadcast('my-channel', {hello: 'world'})
+
+// const sw = swarm(hub)
+
+// sw.on('peer', function (peer, id) {
+//     console.log('connected to a new peer:', id)
+//     peer.on('data', data    )
+//     console.log('total peers:', sw.peers.length)
+// })
+
+// sw.on('disconnect', function (peer, id) {
+//     console.log('disconnected from a peer:', id)
+//     console.log('total peers:', sw.peers.length)
+// })
